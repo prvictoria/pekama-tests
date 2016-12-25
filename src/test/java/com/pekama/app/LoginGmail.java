c16ae589fa1facdb7c7db67fe9925bbe7f382b4b
Vatslau <v375291200656@gmail.com>
2016-12-25 22:42:56 +0300

    initial commits

--- src/test/java/com/pekama/app/LoginGmail.java
+++ src/test/java/com/pekama/app/LoginGmail.java
@@ -4,14 +4,17 @@ import com.codeborne.selenide.Condition;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.appears;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.title;
+import static junit.framework.Assert.assertTrue;
 
 /**
  * Created by VatslauX on 25-Dec-16.
  */
 public class LoginGmail {
+
     @Test
     public void open_page() {
         open("https://mail.google.com/mail/u/0/#inbox");
@@ -19,9 +22,14 @@ public class LoginGmail {
         $(By.name("signIn")).click();
         $(By.id("Passwd")).shouldBe(Condition.visible).sendKeys("123456789qasw1");
         $(By.id("signIn")).shouldBe(Condition.visible).click();
-        //$(By.xpath("//")).title().is(Condition.text("Inbox - testqweeco001@gmail.com - Gmail"));
-
-        //$(title()).shouldBe(Condition.visible).shouldHave(Condition.text("Inbox - testqweeco001@gmail.com - Gmail"));
+        //$(By.xpath("//div[@role='button' and contains(.,'COMPOSE')]")).click();
+        //$(byTitle("Inbox - testqweeco001@gmail.com - Gmail")).waitUntil(appears, 10000); //visible element
+        //$(byTitle("Inbox - testqweeco001@gmail.com - Gmail")).should(exist);
+        //assertEquals("Inbox - testqweeco001@gmail.com - Gmail", title());
+        $(By.xpath("//a[@href='https://accounts.google.com/SignOutOptions?hl=en&continue=https://mail.google.com/mail&service=mail']")).waitUntil(appears, 10000); //visible element
+        //$(By.xpath("//a[@href='https://accounts.google.com/SignOutOptions?hl=en&continue=https://mail.google.com/mail&service=mail']")).shouldHave(attribute("title", "Google Account: Test Account (testqweeco001@gmail.com)"));
+        $(By.xpath("//a[@title='Google Account: Test Account (testqweeco001@gmail.com)']")).click();
+        //assertTrue(true, $(By.xpath("//a[@title='Google Account: Test Account (testqweeco001@gmail.com')"));
 
     };
 }
