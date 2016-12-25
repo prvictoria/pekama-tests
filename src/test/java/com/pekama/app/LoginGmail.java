093f3e779d350ce76b869e0969a734877cade9bd
Vatslau <v375291200656@gmail.com>
2016-12-25 12:33:09 +0300

    initial commits

--- src/test/java/com/pekama/app/LoginGmail.java
+++ src/test/java/com/pekama/app/LoginGmail.java
@@ -1,8 +1,10 @@
 package com.pekama.app;
 
+import com.codeborne.selenide.Condition;
 import org.junit.Test;
+import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selenide.assertNoJavascriptErrors;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 /**
@@ -11,7 +13,11 @@ import static com.codeborne.selenide.Selenide.open;
 public class LoginGmail {
     @Test
     public void open_page() {
-        open("http://gmail.com/");
+        open("https://mail.google.com/mail/u/0/#inbox");
+        $(By.name("Email")).sendKeys("testqweeco001@gmail.com");
+        $(By.name("signIn")).click();
+        $(By.id("Passwd")).shouldBe(Condition.visible).sendKeys("123456789qasw1");
+        $(By.id("signIn")).shouldBe(Condition.visible).click();
 
     };
 }
