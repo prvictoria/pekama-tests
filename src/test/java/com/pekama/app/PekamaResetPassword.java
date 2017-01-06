91f500fa3da2897ca04441d9a698fa1cc29f4874
Vatslau <balashevich.viachaslau.08@student.ehu.lt>
2017-01-06 13:19:19 +0300

    Community dashboard

--- src/test/java/com/pekama/app/PekamaResetPassword.java
+++ src/test/java/com/pekama/app/PekamaResetPassword.java
@@ -4,9 +4,8 @@ import com.codeborne.selenide.Condition;
 import com.pekama.app.draft.LoginGmail;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.Ignore;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.runners.MethodSorters;
 import org.openqa.selenium.By;
 
 
@@ -15,16 +14,17 @@ import static Page.PekamaLogin.*;
 import static Page.PekamaResetPassword.*;
 import static Page.RunConfig.PEKAMA;
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by Viachaslau_Balashevi on 1/3/2017.
  */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class PekamaResetPassword {
     public static String SELECT_HOST = PEKAMA;
     static final Logger logging = LogManager.getLogger(LoginGmail.class);
-    @Rule
 
-    //DOMConfigurator.configure("log4j2.xml");
+
     @Test
     public void openResetPassword() {
         open(urlLogin);
@@ -44,8 +44,8 @@ public class PekamaResetPassword {
         $(By.xpath(RESET_PAGE_ERROR)).shouldBe(Condition.visible).shouldHave(Condition.text(RESET_PAGE_ERROR_MSG));
     }
     @Test
-    @RunIf
     public void validEmailResetPassword() {
+
         open(urlResetPassword);
         sleep(1000);
         $(By.xpath(RESET_PAGE_TITLE)).shouldBe(Condition.visible).shouldHave(Condition.text(RESET_PAGE_TITLE_TEXT));
@@ -53,7 +53,8 @@ public class PekamaResetPassword {
         $(By.xpath(RESET_PAGE_RESET_BTN)).click();
         sleep(1000);
         $(By.xpath(RESET_PAGE_SUCCESS)).shouldBe(Condition.visible).shouldHave(Condition.text(RESET_PAGE_SUCCESS_MSG));
-        logging.info("Success messages displayed, valid email submitted");
+        String testSuccessMsg = $(By.xpath(RESET_PAGE_SUCCESS)).getText();
+        logging.info(testSuccessMsg+ " displayed, valid email submitted");
 
         logging.info("Inbox Email opened");
         logging.info("Mail detected");
@@ -62,6 +63,17 @@ public class PekamaResetPassword {
         logging.info("et up new password page opened");
         logging.info("User logged with new credentials");
 
+    }
+    @Test
+    public void testFailed() {
+
+
+        logging.info("Test obove failed");
+    }
+    @Test
+    public void testPassed() {
+
+        logging.info("Test obove passed");
     }
 
 }
