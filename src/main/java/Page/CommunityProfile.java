ca004136197148ace8a2f76f48277047c406c7f8
Vatslau <balashevich.viachaslau.08@student.ehu.lt>
2017-01-09 19:24:10 +0300

    Profile test

--- src/main/java/Page/CommunityProfile.java
+++ src/main/java/Page/CommunityProfile.java
@@ -1,7 +1,28 @@
 package Page;
-
-/**
- * Created by Viachaslau_Balashevi on 12/29/2016.
- */
 public class CommunityProfile {
+    public static final String PROFILE_SELECT_DEFINING = "";
+    public static final String PROFILE_INPUT_DEFINING = "";
+    public static final String PROFILE_SELECT_JURSDICTION = "";
+    public static final String PROFILE_INPUT_JURSDICTION = "";
+    public static final String PROFILE_SELECT_SERVICE = "";
+    public static final String PROFILE_INPUT_SERVICE = "";
+    public static final String PROFILE_INPUT_PRICE = "";
+    public static final String PROFILE_INPUT_DESCRIPTION = "";
+
+    public static final String PROFILE_MEMBERS = "";
+    public static final String PROFILE_MEMBERS_COUNT = "";
+    public static final String PROFILE_MEMBERS_ROW = "";
+
+    public static final String PROFILE_BTN_DELETE = "";
+    public static final String PROFILE_BTN_EDIT = "";
+    public static final String PROFILE_BTN_ADD = "";
+    public static final String PROFILE_BTN_YES = "";
+    public static final String PROFILE_BTN_NO = "";
+    public static final String PROFILE_BTN_SAVE = "";
+    public static final String PROFILE_BTN_INVITE = "";
+    public static final String PROFILE_BTN_AVATAR_DELETE = "";
+    public static final String PROFILE_BTN_AVATAR_UPLOAD = "";
+
+
+
 }
